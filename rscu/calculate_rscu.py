39bbe28633ebdcf22b825fcdf7686bc77b905017
eteasell <teasella@gmail.com>
2025-07-25 16:02:12 -0700

    fix RSCU structure bug, add t-test

--- rscu/calculate_rscu.py
+++ rscu/calculate_rscu.py
@@ -66,12 +66,55 @@ for record in records:
     if window_rscu.rscu_values == {}:
         print(f"errors for {record.id} window")
     
-    
     values[record.id] = {
-        'full': window_rscu.rscu_values,
-        'window': full_rscu.rscu_values
+        'full': full_rscu.rscu_values,
+        'window': window_rscu.rscu_values
     }
 
-path = PROJ_DIR / "data/rscu_values_filtered.json"
+path = PROJ_DIR / "data/rscu_values.json"
 save_json(values, path)
+
+################ Additionally, format data as a dataframe ##################
+# Dataframe should be indexed on sys_name, with 218 rows, and 122 (2 x 61) columns for each codon (window and background)
+codon_labels = [
+    "GCT-w", "GCT-b", "GCC-w", "GCC-b", "GCA-w", "GCA-b", "GCG-w", "GCG-b",
+    "CGT-w", "CGT-b", "CGC-w", "CGC-b", "CGA-w", "CGA-b", "CGG-w", "CGG-b",
+    "AGA-w", "AGA-b", "AGG-w", "AGG-b", "AAT-w", "AAT-b", "AAC-w", "AAC-b",
+    "GAT-w", "GAT-b", "GAC-w", "GAC-b", "TGT-w", "TGT-b", "TGC-w", "TGC-b",
+    "CAA-w", "CAA-b", "CAG-w", "CAG-b", "GAA-w", "GAA-b", "GAG-w", "GAG-b",
+    "GGT-w", "GGT-b", "GGC-w", "GGC-b", "GGA-w", "GGA-b", "GGG-w", "GGG-b",
+    "CAT-w", "CAT-b", "CAC-w", "CAC-b", "ATT-w", "ATT-b", "ATC-w", "ATC-b",
+    "ATA-w", "ATA-b", "TTA-w", "TTA-b", "TTG-w", "TTG-b", "CTT-w", "CTT-b",
+    "CTC-w", "CTC-b", "CTA-w", "CTA-b", "CTG-w", "CTG-b", "AAA-w", "AAA-b",
+    "AAG-w", "AAG-b", "ATG-w", "ATG-b", "TTT-w", "TTT-b", "TTC-w", "TTC-b",
+    "CCT-w", "CCT-b", "CCC-w", "CCC-b", "CCA-w", "CCA-b", "CCG-w", "CCG-b",
+    "TCT-w", "TCT-b", "TCC-w", "TCC-b", "TCA-w", "TCA-b", "TCG-w", "TCG-b",
+    "AGT-w", "AGT-b", "AGC-w", "AGC-b", "ACT-w", "ACT-b", "ACC-w", "ACC-b",
+    "ACA-w", "ACA-b", "ACG-w", "ACG-b", "TGG-w", "TGG-b", "TAT-w", "TAT-b",
+    "TAC-w", "TAC-b", "GTT-w", "GTT-b", "GTC-w", "GTC-b", "GTA-w", "GTA-b",
+    "GTG-w", "GTG-b"
+]
+
+mat = np.zeros((218,122))
+gene_list = []
+
+for i, gene in enumerate(values.keys()):
+    gene_list.append(gene)
     
+    gene_i = values[gene]
+    
+    window = dict(gene_i['window'])
+    full = gene_i['full']
+    
+    for j, codon in enumerate(window.keys()):
+        mat[i, 2*j] = window[codon]
+        mat[i, 2*j + 1] = full[codon]
+        
+
+mat = np.round(mat,decimals=3)
+df = pd.DataFrame(mat, index=gene_list, columns=codon_labels)
+
+print(df)
+
+df_path = PROJ_DIR / "data/rscu_values.tsv"
+df.to_csv(df_path, sep="\t", index=True)
